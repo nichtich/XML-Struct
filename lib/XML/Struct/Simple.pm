9e2cb24843d171716e45650f748409384d7d8bba
Jakob Voss <voss@gbv.de>
2015-05-22 13:52:59 +0200

    refactored XML::Struct::Simple

--- lib/XML/Struct/Simple.pm
+++ lib/XML/Struct/Simple.pm
@@ -2,45 +2,102 @@ package XML::Struct::Simple;
 use strict;
 use Moo;
 use List::Util qw(first);
+use Scalar::Util qw(reftype blessed);
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
-has root       => (is => 'rw', default => sub { 0 });
-has attributes => (is => 'rw', default => sub { 1 });
-has content    => (is => 'rw', default => sub { 'content' });
-has depth      => (is => 'rw');
+has root => (
+    is => 'rw', 
+    default => sub { 0 },
+);
+
+has attributes => (
+    is => 'rw', 
+    default => sub { 1 }, 
+    coerce => sub { !defined $_[0] or ($_[0] and $_[0] ne 'remove') },
+);
+
+has content => (
+    is => 'rw', 
+    default => sub { 'content' },
+);
+
+has depth => (
+    is => 'rw',
+    coerce => sub { (defined $_[0] and $_[0] >= 0) ? $_[0] : undef },
+);
 
 sub transform {
     my ($self, $element) = @_;
+    
+    my $simple = $self->transform_content($element,0);
 
-    my $attributes = (!defined $self->attributes or $self->attributes);
+    # enforce root for special case <root>text</root>
+    if ($self->root or !ref $simple) {
+        my $root = $self->root !~ /^[+-]?[0-9]+$/ ? $self->root : $element->[0];
+        return { $root => $simple };
+    } else {
+        return $simple;
+    }
+}
 
-    if ($attributes eq 'remove') {
-        $element = removeXMLAttr($element);
-        $attributes = 0;
+# returns a (possibly empty) hash or a scalar
+sub transform_content {
+    my ($self, $element, $depth) = @_;
+    $depth = 0 if !defined $depth;
+
+    if (defined $self->depth and $depth >= $self->depth) {
+        return $element;
+    } elsif ( @$element == 1 ) { # empty tag
+        return { }; 
     }
 
-    if (defined $self->depth and $self->depth !~ /^\+?\d+/) {
-        $self->depth(undef);
+    my $attributes = {};
+    my $children;
+
+    if ( reftype $element->[1] eq 'HASH' ) { # [ $tag, \%attributes, \@children ]
+        $attributes = $element->[1] if $self->attributes;
+        $children   = $element->[2];
+    } else {                                 # [ $tag, \@children ]
+        $children   = $element->[1];
+    }
+    
+    # no element children
+    unless ( first { ref $_ } @$children ) {
+        my $content = join "", @$children;
+        if (!%$attributes) {
+            return $content;
+        } elsif ($content eq '') {
+            return { %$attributes };
+        } else {
+            return { %$attributes, $self->content => $content };
+        }
     }
 
-    if ($self->root) {
-        my $root = $self->root;
-        $root = $element->[0] if $root =~ /^[+-]?[0-9]+$/;
+    my $simple = { map {$_ => [$attributes->{$_}] } keys %$attributes };
 
-        $self->depth($self->depth - 1) if defined $self->depth;
+    foreach my $child ( @$children ) {
+        next unless ref $child; # skip mixed content text
 
-        my $hash = $attributes
-                ? $self->_simple(1, [ dummy => {}, [$element] ], $self->depth)
-                : $self->_simple(0, [ dummy => [$element] ], $self->depth);
+        my $name    = $child->[0];
+        my $content = $self->transform_content($child, $depth+1);
 
-        return { $root => values %$hash };
+        if ( $simple->{$name} ) {
+            push @{$simple->{$name}}, $content;
+        } else {
+            $simple->{$name} = [$content];
+        }
     }
 
-    my $hash = $self->_simple($attributes, $element, $self->depth);
-    $hash = { } unless ref $hash;
+    foreach my $name (keys %$simple) {
+        next if @{$simple->{$name}} != 1;
+        my $c = $simple->{$name}->[0];
+        if (!ref $c or (!blessed $c and reftype $c eq 'HASH')) {
+            $simple->{$name} = $c;
+        }
+    }
 
-    return $hash;
+    return $simple;
 }
 
 sub removeXMLAttr {
@@ -50,50 +107,6 @@ sub removeXMLAttr {
             ? [ $node->[0], [ map { removeXMLAttr($_) } @{$node->[2]} ] ]
             : [ $node->[0] ] ) # empty element
         : $node;               # text node
-}
-
-# hashifies attributes and child elements
-sub _simple {
-    my $self = shift;
-    my $with_attributes = $_[0];
-    my ($children, $attributes) = $with_attributes ? ($_[1]->[2], $_[1]->[1]) : ($_[1]->[1]);
-    my $depth = defined $_[2] ? $_[2] - 1 : undef;
-
-    my $text_only = (first { ref $_ } @$children) ? undef : join("",@$children);
-
-    # empty element or characters only 
-    if (!($attributes and %$attributes) and defined $text_only) {
-        return $text_only ne "" ? $text_only : { };
-    }
-
-    my $hash = { $attributes ? %$attributes : () };
-    $hash->{ $self->content } = $text_only if defined $text_only and @$children;
-
-    foreach my $child ( @$children ) {
-        next unless ref $child; # skip mixed content text
-        if (defined $depth and $depth < 0) {
-            _push_hash( $hash, $child->[0], $child, 1 );
-        } else {
-            _push_hash( $hash, $child->[0] => $self->_simple($with_attributes, $child, $depth) );
-        }
-    }
-
-    return $hash; 
-}
-
-sub _push_hash {
-    my ($hash, $key, $value, $force) = @_;
-
-    if ( exists $hash->{$key} ) {
-        if ((ref $hash->{$key} || '') ne 'ARRAY') {
-            $hash->{$key} = [ $hash->{$key} ];
-        }
-        push @{$hash->{$key}}, $value;
-    } elsif ( $force ) {
-        $hash->{$key} = [ $value ];
-    } else {
-        $hash->{$key} = $value;
-    }
 }
 
 
@@ -108,54 +121,25 @@ XML::Struct::Simple - Transform MicroXML data structures into simple (unordered)
 
 =head1 SYNOPSIS
 
+    my $micro = [ 
+        root => { xmlns => 'http://example.org/' }, 
+        [ '!', [ x => {}, [42] ] ]
+    ];
     my $converter = XML::Struct::Simple->new( root => 'record' );
-    my $struct = [ root => { xmlns => 'http://example.org/' }, 
-                   [ '!', [ x => {}, [42] ] ] ];
-
-    my $simple = $converter->transform( $xml );
+    my $simple = $converter->transform( $micro );
     # { record => { xmlns => 'http://example.org/', x => 42 } }
 
 =head1 DESCRIPTION
 
 This module implements a transformation from structured XML (MicroXML) to
-simple key-value format as known from L<XML::Simple>: Attributes and child
-elements are treated as hash keys with their content as value. Text elements
-without attributes are converted to text and empty elements without attributes
-are converted to empty hashes.
+simple key-value format (SimpleXML) as known from L<XML::Simple>: Attributes
+and child elements are treated as hash keys with their content as value. Text
+elements without attributes are converted to text and empty elements without
+attributes are converted to empty hashes.
 
-L<XML::Struct> can export the function C<simpleXML> for easy use.
-
-=head1 CONFIGURATION
-
-=over
-
-=item root
-
-Keep the root element instead of removing. This corresponds to option
-C<KeepRoot> in L<XML::Simple>. In addition one can set the name of the root
-element if a non-numeric value is passed.
-
-=item content
-
-With option C<simple> enable, text content at elements with attributes is
-parsed into a hash value with this key.  Set to "C<content> by default.
-Corresponds to option C<ContentKey> in L<XML::Simple>.
-
-=item attributes
-
-Assume input without attributes if set to a true value. The special value
-C<remove> will first remove attributes, so the following three are equivalent:
-
-    my @children = (['a'],['b']);
-
-    simpleXML( [ $name => \@children ], attributes => 0 );
-    simpleXML( removeXMLAttr( [ $name => \%attributes, \@children ] ), attributes => 0 );
-    simpleXML( [ $name => \%attributes, \@children ], attributes => 'remove' );
-
-=back
-
-Option C<KeyAttr>, C<ForceArray>, and other fetures of L<XML::Simple> not
-supported (yet).
+L<XML::Struct> can export the function C<simpleXML> for easy use. Function
+C<readXML> and L<XML::Struct::Reader> apply transformation to SimpleXML with
+option C<simple>. 
 
 =head1 METHODS
 
@@ -164,10 +148,50 @@ supported (yet).
 Transform XML given as array reference (MicroXML) to XML as hash reference
 (SimpleXML) as configured.
 
+=head2 transform_content( $element [, $depth ] )
+
+Transform child nodes and attributes of an XML element given as array reference
+at a given depth (C<0> by default). Returns a hash reference, a scalar, or the
+element unmodified.
+
+=head1 CONFIGURATION
+
+=over
+
+=item root
+
+Keep the root element instead of removing. This corresponds to option
+C<KeepRoot> in L<XML::Simple>. In addition a non-numeric value can be used to 
+override the name of the root element. Disabled by default.
+
+=item attributes
+
+Include XML attributes. Enabled by default. The special value C<remove> is
+equivalent to false. Corresponds to option C<NoAttr> in L<XML::Simple>.
+
+=item content
+
+Name of a field to put text content in. Set to "C<content> by default.
+Corresponds to option C<ContentKey> in L<XML::Simple>.
+
+=item depth
+
+Only transform up to a given depth. Set to a negative value by default for
+unlimited depth. Elements below depth are not cloned but copied by reference.
+Depth 0 will return the element unmodified.
+
+=back
+
+Option C<KeyAttr>, C<ForceArray>, and other fetures of L<XML::Simple> not
+supported. Options C<NsExpand> and C<NsStrip> supported in
+L<XML::LibXML::Simple> are not supported yet.
+
 =head2 FUNCTIONS
 
 =head2 removeXMLAttr( $element )
 
 Recursively remove XML attributes from XML given as array reference (MicroXML).
+
+This function is deprecated.
 
 =cut
--- t/struct.t
+++ t/struct.t
@@ -1,10 +1,10 @@
 use strict;
 use Test::More;
 
+diag 'SYNOPSIS of XML::Struct';
+
 my $input = join "\n", '<?xml version="1.0" encoding="UTF-8"?>',
     '<root xmlns="http://example.org/">!<x>42</x></root>','';
-
-diag 'SYNOPSIS of XML::Struct';
 
 use XML::Struct qw(readXML writeXML simpleXML removeXMLAttr);
 
@@ -19,19 +19,5 @@ is_deeply $simple, { record => { xmlns => 'http://example.org/', x => 42 } };
 
 my $xml2 = removeXMLAttr($xml);
 is_deeply $xml2, [ root => [ '!', [ x => [42] ] ] ];
-
-diag 'EXAMPLE of XML::Struct::simpleXML';
-
-my $name = 'foo';
-my %attributes = ( a => 42 );
-my @children = (['a'],['b']);
-
-my $a = simpleXML( [ $name => \@children ], attributes => 0 );
-my $b = simpleXML( removeXMLAttr( [ $name => \%attributes, \@children ] ), attributes => 0 );
-my $c = simpleXML( [ $name => \%attributes, \@children ], attributes => 'remove' );
-
-is_deeply $a, { a => {}, b => {} };
-is_deeply $a, $b;
-is_deeply $b, $c;
 
 done_testing;
